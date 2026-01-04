1e38f4af7a3aa9757706dee4b40bf5f0078a1e3a
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2026-01-04 17:11:05 +0900

    Changes to be committed: 블록체인 구현 	modified:   package.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,32 @@ class Block implements BlockShape {
         return crypto.createHash("sha256").update(toHash).digest("hex")
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = []
+    }
+
+    private getPrevHash() : string {
+        if (this.blocks.length === 0) return ""; // 첫 해시가 없으니 빈 문자열
+        return this.blocks[this.blocks.length -1].hash; // 마지막 블록의 해시값 반환
+    }
+
+    public addBlock(data: string) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock); // 블록체인에 새 블록 추가
+    }
+
+    public getBlocks() {
+        return [...this.blocks]; // 블록체인 복사본 반환 (변조 방지)
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("첫 번째 블록");
+blockchain.addBlock("두 번째 블록");
+blockchain.addBlock("세 번째 블록");
+
+console.log(blockchain.getBlocks());
